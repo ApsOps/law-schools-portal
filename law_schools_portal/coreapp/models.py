b7a1fd31eed3012b4db390814d4d4eef0f69f334
Amanpreet Singh <aps.sids@gmail.com>
2014-11-04 18:24:24 +0530

    Update models

--- law_schools_portal/coreapp/models.py
+++ law_schools_portal/coreapp/models.py
@@ -4,21 +4,6 @@ from cities_light.models import City, Country
 # Create your models here.
 
 
-class Address(models.Model):
-
-    class Meta:
-        verbose_name = "Address"
-        verbose_name_plural = "Addresses"
-
-    def __unicode__(self):
-        pass
-
-    address_line1 = models.CharField("Address line 1", max_length=45)
-    address_line2 = models.CharField("Address line 2", max_length=45,
-                                     blank=True)
-    postal_code = models.CharField("Postal Code", max_length=10)
-
-
 class LawSchool(models.Model):
 
     class Meta:
@@ -35,6 +20,11 @@ class LawSchool(models.Model):
     fb_link = models.URLField(blank=True)
     twitter_link = models.URLField(blank=True)
     linkedin_link = models.URLField(blank=True)
+    address_line1 = models.CharField("Address line 1", max_length=45,
+                                     blank=True, null=True)
+    address_line2 = models.CharField("Address line 2", max_length=45,
+                                     blank=True, null=True)
+    postal_code = models.IntegerField("Postal Code", max_length=10,
+                                      blank=True, null=True)
     city = models.ForeignKey(City)
     country = models.ForeignKey(Country)
-    address = models.ForeignKey(Address, blank=True)
